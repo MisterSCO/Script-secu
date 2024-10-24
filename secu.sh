f1cae4654c04cdc164ceecfd4ecdc43e19011f46
MisterSCO <l.lecomte77@gmail.com>
2024-10-24 22:18:39 +0200

    suppression des ip blanche, ajout de quelques fonctions

--- secu.sh
+++ secu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Arret du script en cas d'erreur
+set -e
+
 # Vérification des droits d'accès
 if [ "$(id -u)" != "0" ]; then
     echo "Ce script doit être exécuté en tant que root!"
@@ -10,6 +13,15 @@ fi
 apt update
 apt install -y vim tree curl wget openssh-server ufw git
 
+# Fonction pour vérifier l'installation des paquets
+check_pkg() {
+    if ! dpkg -l | grep -q "$1"; then
+        echo "Le paquet $1 n'est pas installé."
+    else
+        echo "Le paquet $1 est déjà installé."
+    fi
+}
+
 # Vérification de l'installation des paquets
 check_pkg vim
 check_pkg tree
@@ -19,25 +31,22 @@ check_pkg wget
 check_pkg openssh-server
 check_pkg ufw
 
+# Ajout du usr/sbin dans le PATH si il n'y est pas
+if [[ ":$PATH:" != *":/usr/sbin:"* ]]; then
+    export PATH="$PATH:/usr/sbin"
+fi
+
+# Vérification si le service SSH écoute sur le port 22
+if ss -tuln | grep -q ':22'; then
+    echo "Le service SSH écoute sur le port 22."
+else
+    echo "Le service SSH ne écoute pas sur le port 22."
+    echo "Vérifiez si le service SSH est en cours d'exécution et s'il est correctement configuré."
+    exit 1
+fi
+
 # Configuration du service SSH
 systemctl enable ssh --now
-
-# Désactivation de l'accès root via SSH
-sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-systemctl ssh restart
-
-# Désactivation de l'authentification par mot de passe
-sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-systemctl ssh restart
-
-# Liste blanche d'adresses IP autorisées
-allowed_ips="192.168.1.0/24 10.0.0.0/8"
-
-# Configuration de la liste blanche d'adresses IP
-echo "Match Address $allowed_ips" >> /etc/ssh/sshd_config
-echo "  PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "  PermitRootLogin no" >> /etc/ssh/sshd_config
-service ssh restart
 
 # Configuration du répertoire SSH
 if [ ! -d ~/.ssh ]; then
@@ -49,6 +58,18 @@ if [ ! -f ~/.ssh/authorized_keys ]; then
     touch ~/.ssh/authorized_keys
 fi
 chmod 600 ~/.ssh/authorized_keys
+
+
+
+# Désactivation de l'accès root via SSH
+sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+systemctl restart ssh 
+
+# Désactivation de l'authentification par mot de passe
+sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+systemctl restart ssh
+
+
 
 # Génération des clés SSH
 if [ ! -f ~/.ssh/id_rsa ]; then
@@ -63,7 +84,7 @@ if ! getent group ssh_users > /dev/null; then
         groupadd -g 1001 ssh_users
         echo "AllowGroups ssh_users" >> /etc/ssh/sshd_config
         echo "DenyGroups *" >> /etc/ssh/sshd_config
-        service ssh restart
+        systemctl restart ssh
     fi
 fi
 
