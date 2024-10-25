9386dbed827a01d5f4cf10ada9628dcadd40e82e
MisterSCO <l.lecomte77@gmail.com>
2024-10-25 12:09:10 +0200

    Version utilisé pour la soutenance

--- secu.sh
+++ secu.sh
@@ -3,17 +3,17 @@
 # Arret du script en cas d'erreur
 set -e
 
-# Vérification des droits d'accès
-if [ "$(id -u)" != "0" ]; then
-    echo "Ce script doit être exécuté en tant que root!"
+# Vérifier si l'utilisateur a les droits root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Erreur : Ce script nécessite les droits root."
     exit 1
 fi
 
 # Installation des paquets nécessaires
 apt update
-apt install -y vim tree curl wget openssh-server ufw git
+apt install -y vim tree curl wget openssh-server ufw git unattended-upgrades
 
-# Fonction pour vérifier l'installation des paquets
+# Fonction pour vérifier si les paquets sont installés
 check_pkg() {
     if ! dpkg -l | grep -q "$1"; then
         echo "Le paquet $1 n'est pas installé."
@@ -30,8 +30,9 @@ check_pkg git
 check_pkg wget
 check_pkg openssh-server
 check_pkg ufw
+check_pkg unattended-upgrades
 
-# Ajout du usr/sbin dans le PATH si il n'y est pas
+# Ajout du usr/sbin dans le PATH si il n'y est pas, le temps du script
 if [[ ":$PATH:" != *":/usr/sbin:"* ]]; then
     export PATH="$PATH:/usr/sbin"
 fi
@@ -60,21 +61,18 @@ fi
 chmod 600 ~/.ssh/authorized_keys
 
 
-
 # Désactivation de l'accès root via SSH
 sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-systemctl restart ssh 
 
 # Désactivation de l'authentification par mot de passe
 sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-systemctl restart ssh
-
 
 
 # Génération des clés SSH
 if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 fi
+
 
 # Création du groupe ssh_users si possible
 if ! getent group ssh_users > /dev/null; then
@@ -84,9 +82,10 @@ if ! getent group ssh_users > /dev/null; then
         groupadd -g 1001 ssh_users
         echo "AllowGroups ssh_users" >> /etc/ssh/sshd_config
         echo "DenyGroups *" >> /etc/ssh/sshd_config
-        systemctl restart ssh
     fi
 fi
+
+systemctl restart ssh
 
 # Configuration du pare-feu UFW
 ufw default deny incoming
@@ -96,5 +95,34 @@ ufw allow https
 ufw enable
 
 
+# Configuration des mises à jour automatiques des paquets de sécurité
+echo "Configuration des mises à jour automatiques des paquets de sécurité..."
+
+
+
+#  Vérifie la destribution et la version
+distro_id=$(lsb_release -si)
+distro_codename=$(lsb_release -sc)
+
+# Vérifier et ajouter la configuration pour les mises à jour automatiques
+if ! grep -q "APT::Periodic::Update-Package-Lists" /etc/apt/apt.conf.d/20auto-upgrades; then
+    echo "APT::Periodic::Update-Package-Lists \"1\";" >> /etc/apt/apt.conf.d/20auto-upgrades
+fi
+
+# Vérification des mise a jour non surveillées
+if ! grep -q "APT::Periodic::Unattended-Upgrade" /etc/apt/apt.conf.d/20auto-upgrades; then
+    echo "APT::Periodic::Unattended-Upgrade \"1\";" >> /etc/apt/apt.conf.d/20auto-upgrades
+fi
+
+
+# Vérification des origines autorisées pour les mise à jour automatique
+if ! grep -q "Unattended-Upgrade::Allowed-Origins" /etc/apt/apt.conf.d/50unattended-upgrades; then
+    echo "Unattended-Upgrade::Allowed-Origins {" >> /etc/apt/apt.conf.d/50unattended-upgrades
+    echo "    \"${distro_id}:${distro_codename}-security\";" >> /etc/apt/apt.conf.d/50unattended-upgrades
+    echo "};" >> /etc/apt/apt.conf.d/50unattended-upgrades
+fi
+
+# Vérifier le service
+systemctl enable unattended-upgrades
 
 echo "Script terminé avec succès !"
